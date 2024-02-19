6463eb3e7ca6d35689f2f4681e529104d0320e6b
Alberto Puggioni <124665692+AlbertoPuggioni@users.noreply.github.com>
2024-02-19 09:27:25 +0100

    Add files via upload

---
+++ prova/cms/script.js
@@ -0,0 +1,21 @@
+// Pulsante preview
+document.getElementById('previewButton').addEventListener('click', function() {
+    // Contenuto modificato in CKEditor
+    var editedContent = CKEDITOR.instances['editable-content'].getData();
+    // Visualizzazione del contenuto modificato
+    document.getElementById('previewArea').innerHTML = editedContent;
+    // Area di preview
+    document.getElementById('previewArea').style.display = 'block';
+});
+
+// Inizializzazione di CKEditor per il contenuto
+CKEDITOR.inline('editable-content', {
+    extraPlugins: 'sourcedialog',
+    allowedContent: true
+});
+// Inizializzazione di CKEditor per il titolo
+CKEDITOR.inline('editable-title', {
+    extraPlugins: 'sourcedialog',
+    allowedContent: true
+});
+
