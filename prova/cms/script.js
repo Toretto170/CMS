37ba8b04461fba29f0caf1d03550f9cf5359d106
Francesco Messana <messanafrancesco02@gmail.com>
2024-02-21 10:59:47 +0100

    Funzione Scarica

--- prova/cms/script.js
+++ prova/cms/script.js
@@ -25,3 +25,20 @@ function toggleDarkMode() {
     body.classList.toggle("dark-mode");
 }
 
+//Download del file sorgente come html
+function Scarica(){
+
+    var divhtml = document.getElementsByClassName("cke_dialog_ui_input_textarea")[0];
+    var testo = divhtml.querySelector("textarea").value;
+    var blob = new Blob([testo], { type: 'text/plain' });
+            
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    link.download = 'testo_scaricato.html';
+    
+    document.body.appendChild(link);
+    
+    link.click();
+    
+    document.body.removeChild(link);
+}
