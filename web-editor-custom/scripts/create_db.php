46679668b81fb7dff6e865b628be2087250dee17
Alberto Puggioni <124665692+AlbertoPuggioni@users.noreply.github.com>
2024-03-11 15:34:03 +0100

    Update create_db.php

--- web-editor-custom/scripts/create_db.php
+++ web-editor-custom/scripts/create_db.php
@@ -1,7 +1,17 @@
 <?php
 session_start();
 
-include("../scripts/connection_db.php");
+// Connessione al db
+$host = '127.0.0.1';
+$username = 'root';
+$password = '';
+$database = 'login';
+
+// Check della connessione
+$conn = new mysqli($host, $username, $password);
+if ($conn->connect_error) {
+    die('Connessione fallita : ' . $conn->connect_error);
+}
 
 // Creazione del db
 $sql_create_database = "CREATE DATABASE IF NOT EXISTS $database";
@@ -16,13 +26,13 @@ $conn->select_db($database);
 
 // Creazione della tabella 'users'
 $sql_create_users_table = "CREATE TABLE IF NOT EXISTS users (
-            id BIGINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL,
             password VARCHAR(255) NOT NULL
             )";
 
-// Controllo se la tabella users è stata creata con successo oppure no
+// check per vedere se la tabella users è stata creata con successo oppure no
 if ($conn->query($sql_create_users_table) === TRUE) {
     echo "Tabella 'users' creata con successo.\r\n";
 } else {
@@ -31,21 +41,20 @@ if ($conn->query($sql_create_users_table) === TRUE) {
 
 // Creazione della tabella 'templates'
 $sql_create_templates_table = "CREATE TABLE IF NOT EXISTS templates (
-            id BIGINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             html LONGTEXT NOT NULL,
             css LONGTEXT NOT NULL,
             reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            user_id INT(6) UNSIGNED,
+            user_id BIGINT UNSIGNED,
             FOREIGN KEY (user_id) REFERENCES users(id)
             )";
 
-// Controllo se la tabella templates è stata creata con successo oppure no
+// check per vedere se la tabella templates è stata creata con successo
 if ($conn->query($sql_create_templates_table) === TRUE) {
     echo "Tabella 'templates' creata con successo.\r\n";
 } else {
     echo "Errore nella creazione della tabella 'templates': " . $conn->error;
 }
 
-// chiusura della connessione con il db
+// chiusura della connessione
 $conn->close();
-
