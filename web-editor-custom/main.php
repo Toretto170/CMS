5f2cda63482e1cf1b1d70d4d4752f34b33c1d7f8
Francesco Messana <messanafrancesco02@gmail.com>
2024-03-13 15:47:34 +0100

    gestione cookies

--- web-editor-custom/main.php
+++ web-editor-custom/main.php
@@ -1,5 +1,23 @@
 <!-- PUNTO DI PARTENZA DEI FILE -->
 
+<?php
+session_start();
+
+// Cancella tutte le variabili di sessione
+$_SESSION = array();
+
+// Cancella il cookie di sessione
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Cancella la sessione
+session_destroy();
+?>
 <!-- PAGINA DI LOGIN / REGISTRAZIONE -->
 <!DOCTYPE html>
 <html lang="en">
