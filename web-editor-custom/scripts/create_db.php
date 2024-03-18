38161255a14f46d7f0ff9e5433a99299865ee6b6
Alberto <puggioni.alberto@gmail.com>
2024-03-18 14:24:30 +0100

    corretto il bigint in create_db

--- web-editor-custom/scripts/create_db.php
+++ web-editor-custom/scripts/create_db.php
@@ -27,7 +27,7 @@ $conn->select_db($database);
 
 // Creazione della tabella 'users'
 $sql_create_users_table = "CREATE TABLE IF NOT EXISTS users (
-            id BIGINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             username VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL,
             password VARCHAR(255) NOT NULL
@@ -43,7 +43,7 @@ if ($conn->query($sql_create_users_table) === TRUE) {
 
 // Creazione della tabella 'templates'
 $sql_create_templates_table = "CREATE TABLE IF NOT EXISTS templates (
-            id BIGINT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
             html LONGTEXT NOT NULL,
             css LONGTEXT NOT NULL,
             reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
