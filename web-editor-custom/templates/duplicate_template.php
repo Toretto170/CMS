70e62e44b9b4a15efcc175abdcf4232b5b44901b
Alberto <puggioni.alberto@gmail.com>
2024-03-22 09:50:45 +0100

    fix del duplicate del name del template

--- web-editor-custom/templates/duplicate_template.php
+++ web-editor-custom/templates/duplicate_template.php
@@ -3,11 +3,10 @@ session_start();
 $_SESSION['success_message'] = "Template successfully duplicated!";
 $_SESSION['error_message'] = "Cannot duplicate the template";
 
+include ("../modules/authentication_user.php");
+include ("../modules/connection_db.php");
 
-include("../modules/authentication_user.php");
-include("../modules/connection_db.php");
-
-if(isset($_GET['id'])) {
+if (isset ($_GET['id'])) {
     $template_id = $_GET['id'];
 
     // Verifica che il template appartenga all'utente autenticato
@@ -18,9 +17,9 @@ if(isset($_GET['id'])) {
     $check_stmt->execute();
     $check_result = $check_stmt->get_result();
 
-    if($check_result->num_rows == 1) {
-        // Estrai il template originale
-        $fetch_query = "SELECT html, css FROM templates WHERE id=?";
+    if ($check_result->num_rows == 1) {
+        // Estrai il template originale e il suo nome
+        $fetch_query = "SELECT name, html, css FROM templates WHERE id=?";
         $fetch_stmt = $conn->prepare($fetch_query);
         $fetch_stmt->bind_param("i", $template_id);
         $fetch_stmt->execute();
@@ -28,28 +27,27 @@ if(isset($_GET['id'])) {
 
         if ($template_result->num_rows == 1) {
             $template_row = $template_result->fetch_assoc();
+            $template_name = $template_row['name'];
             $html_content = $template_row['html'];
             $css_content = $template_row['css'];
 
-            // Inserisci un nuovo record con il contenuto del template originale
-            $insert_query = "INSERT INTO templates (user_id, html, css) VALUES (?, ?, ?)";
+            // Inserisci un nuovo record con il contenuto e il nome del template originale
+            $insert_query = "INSERT INTO templates (name, user_id, html, css) VALUES (?, ?, ?, ?)";
             $insert_stmt = $conn->prepare($insert_query);
-            $insert_stmt->bind_param("iss", $user_id, $html_content, $css_content);
+            $insert_stmt->bind_param("siss", $template_name, $user_id, $html_content, $css_content);
             $insert_stmt->execute();
 
             if ($insert_stmt->affected_rows == 1) {
                 $_SESSION['success_message'] = "Template successfully duplicated!";
-                // Alert per la registrazione avvenuta con successo
-                echo "<script>alert('Template successfully duplicated');
-                    window.location.href = 'collection_templates.php'</script>";
-                exit(); // Assicurati di uscire dallo script dopo l'header
-
+                // Redirect alla pagina di raccolta dei template
+                header("Location: collection_templates.php");
+                exit();
             } else {
                 $_SESSION['error_message'] = "Cannot duplicate the template";
-                // Alert per la registrazione avvenuta con successo
-                echo "<script>alert('Cannot duplicate the template');</script>";
+                // Redirect alla pagina di raccolta dei template
+                header("Location: collection_templates.php");
+                exit();
             }
-
         }
     }
 }
