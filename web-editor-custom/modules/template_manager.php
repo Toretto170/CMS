ce6be97285d5a80a89af3458844949c6493bf555
Alberto <puggioni.alberto@gmail.com>
2024-03-22 13:50:39 +0100

    controllo che richiede l'inserimento del nome per il template

--- web-editor-custom/modules/template_manager.php
+++ web-editor-custom/modules/template_manager.php
@@ -1,9 +1,8 @@
 <?php
-
-include("connection_db.php");
+include ("connection_db.php");
 
 // Se l'ID del template non è fornito, crea un nuovo template e ottieni l'ID
-if (!isset($_GET['id'])) {
+if (!isset ($_GET['id'])) {
     $user_id = $_SESSION['user_id'];
     $sql_check_template = "SELECT id FROM templates WHERE html='' AND css='' AND user_id='$user_id'";
     $result_check_template = $conn->query($sql_check_template);
@@ -78,33 +77,25 @@ if ($result->num_rows != 1) {
 $template_data = $result->fetch_assoc();
 
 // Se i dati del form sono stati inviati, esegui l'aggiornamento del template
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST) && isset($_POST['html']) && isset($_POST['css']) && isset($_POST['name']) && !empty($_POST['html']) && !empty($_POST['css'])) {
-    // Assicurati che i dati siano stati inviati correttamente
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset ($_POST) && isset ($_POST['html']) && isset ($_POST['css']) && isset ($_POST['name']) && !empty ($_POST['html']) && !empty ($_POST['css'])) {
+    // Condizione per controllare se è stato fornito un nome per il template
+    if (empty ($_POST['name'])) {
+        // Se non è stato fornito un nome, allora manda un alert che chiede di inserire un nome per il template prima del salvataggio e non lo inserisce nel db
+        echo "Please enter a name for the template";
+        exit;
+    }
+
+    // Check per vedere che i dati siano stati inseriti correttamente
     $html = $_POST['html'];
     $css = $_POST['css'];
     $templateName = $_POST['name'];
+
     // Escape dei dati prima dell'inserimento nel database per evitare SQL injection
     $html = mysqli_real_escape_string($conn, $html);
     $css = mysqli_real_escape_string($conn, $css);
     $templateName = mysqli_real_escape_string($conn, $templateName);
 
-    // Verifica se è stato fornito un nuovo nome del template, altrimenti mantieni il nome esistente
-    if(empty($templateName)) {
-        // Query per ottenere il nome del template esistente
-        $sql_get_template_name = "SELECT name FROM templates WHERE id=?";
-        $stmt = $conn->prepare($sql_get_template_name);
-        $stmt->bind_param("i", $template_id);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $existingTemplateName = $result->fetch_assoc()['name'];
-
-        // Se il nome del template esistente è valido, utilizzalo
-        if(!empty($existingTemplateName)) {
-            $templateName = $existingTemplateName;
-        }
-    }
-
-    // Esegui un'operazione di aggiornamento del template nel database
+    // Aggiornamento dei template sul db
     $sql_update_data = "UPDATE templates SET html=?, css=?, name=?, reg_date=NOW() WHERE id=? AND user_id=?";
     $stmt = $conn->prepare($sql_update_data);
     $stmt->bind_param("sssii", $html, $css, $templateName, $template_id, $_SESSION['user_id']);
