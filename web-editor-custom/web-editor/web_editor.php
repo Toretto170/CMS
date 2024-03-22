6cde44a714e10d9178b08678ba6afb3c3a64fdc1
Alberto <puggioni.alberto@gmail.com>
2024-03-22 09:41:05 +0100

    aggiunta la logica che gestisce il salvataggio del template (anche il nome)

--- web-editor-custom/web-editor/web_editor.php
+++ web-editor-custom/web-editor/web_editor.php
@@ -32,80 +32,83 @@ include("../modules/template_manager.php");
 </head>
 
 <body>
-    <div id="top-bar">
-        <a href="../pages/home.php"><img src="../img/home.png" id="home"></a>
-        <img src="../img/save.png" id="save">
-        <div class="user">
-            <img src="../img/user.png" id="user">
-            <div class="user-menu">
-                <ul class="hover-menu">
-                    <li><a href="../pages/logout.php">Logout</a></li>
-                </ul>
-            </div>
+<div id="top-bar">
+    <a href="../pages/home.php"><img src="../img/home.png" id="home"></a>
+    <img src="../img/save.png" id="save">
+    <input type="text" id="template-name" placeholder="Enter template name">
+    <div class="user">
+        <img src="../img/user.png" id="user">
+        <div class="user-menu">
+            <ul class="hover-menu">
+                <li><a href="../pages/logout.php">Logout</a></li>
+            </ul>
         </div>
-
-        <!-- BLOCCO PER L'UPLOAD
-    <button id="load">Load your Code</button>
-        <div id="code-input">
-            <textarea id="html-input" placeholder="paste your html here"></textarea>
-            <textarea id="css-input" placeholder="paste your css here"></textarea>
-            <button id="apply-code"> Apply code</button>
-        </div> -->
-    </div>
-    <div id="gjs">
-        <h1 id="title">Web Editor</h1>
     </div>
 
-    <div id="blocks"></div>
+    <!-- BLOCCO PER L'UPLOAD
+<button id="load">Load your Code</button>
+    <div id="code-input">
+        <textarea id="html-input" placeholder="paste your html here"></textarea>
+        <textarea id="css-input" placeholder="paste your css here"></textarea>
+        <button id="apply-code"> Apply code</button>
+    </div> -->
+</div>
+<div id="gjs">
+    <h1 id="title">Web Editor</h1>
+</div>
 
-    <script src="web_editor_script.js"> </script>
-    <script>
-        // Carica il template nel GrapesJS
-        var html = `<?php echo $template_data['html']; ?>`;
-        var css = `<?php echo $template_data['css']; ?>`;
-        editor.setComponents(html);
-        editor.setStyle(css);
+<div id="blocks"></div>
 
-        // Aggiunge l'evento click al pulsante "Save"
-        $('#save').on('click', function () {
-            var html = editor.getHtml();
-            var css = editor.getCss();
+<script src="web_editor_script.js"> </script>
+<script>
+    // Carica il template nel GrapesJS
+    var html = `<?php echo $template_data['html']; ?>`;
+    var css = `<?php echo $template_data['css']; ?>`;
+    editor.setComponents(html);
+    editor.setStyle(css);
 
-            // Effettua una richiesta AJAX per salvare o aggiornare il template nel database
-            $.ajax({
-                url: 'web_editor.php?id=<?php echo $template_id; ?>',
-                method: 'POST',
-                data: { html: html, css: css },
-                success: function (response) {
-                    alert(response);
-                },
-                error: function (xhr, status, error) {
-                    console.error(xhr.responseText);
-                }
-            });
+    // Aggiunge l'evento click al pulsante "Save"
+    $('#save').on('click', function () {
+        var html = editor.getHtml();
+        var css = editor.getCss();
+        var templateName = $('#template-name').val(); // Ottieni il nome del template dall'input
+
+        // Effettua una richiesta AJAX per salvare o aggiornare il template nel database
+        $.ajax({
+            url: 'web_editor.php?id=<?php echo $template_id; ?>',
+            method: 'POST',
+            data: { html: html, css: css, name: templateName }, // Includi il nome del template nella richiesta
+            success: function (response) {
+                alert(response);
+            },
+            error: function (xhr, status, error) {
+                console.error(xhr.responseText);
+            }
         });
+    });
 
-        /*
-            CODICE PER L'UPLOAD
-        $('#load').on('click', function () {
-            $('#code-input').show();
-        });
 
-        // Aggiunge l'evento click al pulsante "Apply Code"
-        $('#apply-code').on('click', function () {
-            var htmlCode = $('#html-input').val();
-            var cssCode = $('#css-input').val();
+    /*
+        CODICE PER L'UPLOAD
+    $('#load').on('click', function () {
+        $('#code-input').show();
+    });
 
-            // Applica il codice HTML e CSS al web editor
-            editor.setComponents(htmlCode);
-            editor.setStyle(cssCode);
+    // Aggiunge l'evento click al pulsante "Apply Code"
+    $('#apply-code').on('click', function () {
+        var htmlCode = $('#html-input').val();
+        var cssCode = $('#css-input').val();
 
-            // Nasconde l'area di inserimento del codice
-            $('#code-input').hide();
-        });
+        // Applica il codice HTML e CSS al web editor
+        editor.setComponents(htmlCode);
+        editor.setStyle(cssCode);
 
-         */
-    </script>
+        // Nasconde l'area di inserimento del codice
+        $('#code-input').hide();
+    });
+
+     */
+</script>
 </body>
 
 </html>
