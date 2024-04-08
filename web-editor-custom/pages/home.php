0d21a7405a134c343222e149a64e1033411093e4
Alberto <puggioni.alberto@gmail.com>
2024-04-08 12:08:28 +0200

    fix generali

--- web-editor-custom/pages/home.php
+++ web-editor-custom/pages/home.php
@@ -2,8 +2,8 @@
 global $conn;
 session_start();
 
-include ("../modules/connection_db.php");
-include ("../modules/authentication_user.php");
+include("../modules/connection_db.php");
+include("../modules/authentication_user.php");
 
 // query per ottenere l'username e mostrarlo poi sulla landing page
 $user_id = $_SESSION['user_id'];
@@ -26,32 +26,34 @@ $stmt->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <link rel="stylesheet" href="home_style.css">
 </head>
 <body>
 <header>
     <div class="navbar">
         <ul class="links">
-            <li> <a href="home.php">Home</a></li>
-            <li> <a href="../web-editor/web_editor.php">Web Editor</a></li>
-            <li> <a href="../templates/collection_templates.php">Collection Templates</a></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="../web-editor/web_editor.php">Web Editor</a></li>
+            <li><a href="../templates/collection_templates.php">Collection Templates</a></li>
         </ul>
         <div class="user">
-    <img src="../img/user.png" id="user">
-    <div class="user-menu">
-        <ul class="hover-menu">
-            <li><a href="./logout.php">Logout</a></li>
-        </ul>
-    </div>
-    </div>
+            <img src="../img/user.png" id="user">
+            <div class="user-menu">
+                <ul class="hover-menu">
+                    <li><a href="./logout.php">Logout</a></li>
+                </ul>
+            </div>
+        </div>
         <div class="toggle_btn">
             <i class="fa-solid fa-bars"></i>
         </div>
         <div class="dropdown_menu">
-            <li> <a href="home.php">Home</a></li>
-            <li> <a href="../web-editor/web_editor.php">Web Editor</a></li>
-            <li> <a href="../templates/collection_templates.php">Collection Templates</a></li>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="../web-editor/web_editor.php">Web Editor</a></li>
+            <li><a href="../templates/collection_templates.php">Collection Templates</a></li>
             <li><a href="./logout.php">Logout</a></li>
         </div>
     </div>
@@ -61,7 +63,8 @@ $stmt->close();
     <section id="home">
         <h1> Welcome aboard, <?php echo $username; ?>! </h1>
         <p>
-            We're thrilled to have you join our team<br/>We can't wait to see the amazing things we'll achieve together. <br/>
+            We're thrilled to have you join our team<br/>We can't wait to see the amazing things we'll achieve together.
+            <br/>
         </p>
     </section>
 </main>
@@ -71,7 +74,7 @@ $stmt->close();
     const toggleBtnIcon = document.querySelector('.toggle_btn i')
     const dropDownMenu = document.querySelector('.dropdown_menu')
 
-    toggleBtn.onclick = function(){
+    toggleBtn.onclick = function () {
         dropDownMenu.classList.toggle('open');
         const isOpen = dropDownMenu.classList.contains('open')
 
