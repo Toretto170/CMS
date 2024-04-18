841e92e9b727d8886e3e1903e616db81eb43929b
Alberto Puggioni <124665692+AlbertoPuggioni@users.noreply.github.com>
2024-04-18 17:06:16 +0200

    Update template_manager.php

--- web_editor_with_grapesjs/modules/template_manager.php
+++ web_editor_with_grapesjs/modules/template_manager.php
@@ -5,16 +5,15 @@ include("connection_db.php");
 // Se l'ID del template non è fornito, crea un nuovo template e ottieni l'ID
 if (!isset($_GET['id'])) {
     $user_id = $_SESSION['user_id'];
-    $sql_check_template = "SELECT id FROM templates WHERE html='' AND css='' AND user_id='$user_id'";
+    $sql_check_template = "SELECT id FROM templates WHERE html='' AND css='' AND user_id='$user_id' AND imgURL=''";
     $result_check_template = $conn->query($sql_check_template);
-
 
     if ($result_check_template && $result_check_template->num_rows > 0) {
         $row = $result_check_template->fetch_assoc();
         $template_id = $row['id'];
     } else {
         $default_name = "Template";
-        $sql = "INSERT INTO templates (html, css, name, user_id, reg_date) VALUES ('', '', ?, ?, NOW())";
+        $sql = "INSERT INTO templates (html, css, name, user_id, reg_date, imgURL) VALUES ('', '', ?, ?, NOW(), '')";
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("si", $default_name, $_SESSION['user_id']);
         $stmt->execute();
@@ -26,17 +25,15 @@ if (!isset($_GET['id'])) {
 }
 
 // Preleva il template corrispondente dall'ID fornito
-$sql = "SELECT html, css, name FROM templates WHERE id=?";
+$sql = "SELECT html, css, name, imgURL FROM templates WHERE id=?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $template_id);
 $stmt->execute();
 $result = $stmt->get_result();
-
-
 $template_data = $result->fetch_assoc();
 
 // Se i dati del form sono stati inviati, esegui l'aggiornamento del template
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST) && isset($_POST['html']) && isset($_POST['css']) && isset($_POST['name']) && !empty($_POST['html']) && !empty($_POST['css'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['html']) && isset($_POST['css']) && isset($_POST['name']) && !empty($_POST['html']) && !empty($_POST['css'])) {
     // Assicurati che il nome del template sia stato fornito
     if (empty($_POST['name'])) {
         // Mostra un alert e interrompi l'esecuzione dello script
@@ -48,6 +45,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST) && isset($_POST['html'
     $html = $_POST['html'];
     $css = $_POST['css'];
     $templateName = $_POST['name'];
+    $imgURL = $_POST['imgURL'];
 
     // Escape dei dati prima dell'inserimento nel database per evitare SQL injection
     $html = mysqli_real_escape_string($conn, $html);
@@ -55,13 +53,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST) && isset($_POST['html'
     $templateName = mysqli_real_escape_string($conn, $templateName);
 
     // Esegui un'operazione di aggiornamento del template nel database
-    $sql_update_data = "UPDATE templates SET html=?, css=?, name=?, reg_date=NOW() WHERE id=? AND user_id=?";
+    $sql_update_data = "UPDATE templates SET html=?, css=?, name=?, reg_date=NOW(), imgURL = ? WHERE id=? AND user_id=?";
     $stmt = $conn->prepare($sql_update_data);
-    $stmt->bind_param("sssii", $html, $css, $templateName, $template_id, $_SESSION['user_id']);
+    $stmt->bind_param("ssssii", $html, $css, $templateName, $imgURL, $template_id, $_SESSION['user_id']);
     if ($stmt->execute()) {
         echo "Template successfully saved. \r\n";
     } else {
-        echo 'Errore: ' . $conn->error;
+        echo 'Error: ' . $conn->error;
     }
     exit;
 }
